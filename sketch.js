41334f4cf9c8dc443073468e26497f3491203a31
ArkaDey12 <122989401+ArkaDey12@users.noreply.github.com>
2023-01-18 19:44:28 +0530

    e
    e

---
+++ sketch.js
@@ -0,0 +1,32 @@
+
+var box 
+        
+function setup() {
+  createCanvas(1250,500);
+  
+  box = createSprite (100,100,20,50)
+
+}
+
+function draw() 
+{
+  background(30);
+  if(keyIsDown(RIGHT_ARROW)){
+    box.position.x = box.position.x +5
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    box.position.x = box.position.x -5
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    box.position.y = box.position.y +5
+  }
+  if(keyIsDown(UP_ARROW)){
+    box.position.y = box.position.y -5
+  }
+  drawSprites()
+}
+
+
+
+
+
